188ec82df7d1ed6c1565c44ec64873108f06ffda
Noor Ul Islam <noorulislam770@gmail.com>
2023-04-16 14:31:58 +0500

    added register and login functionality

--- btre/accounts/views.py
+++ btre/accounts/views.py
@@ -1,20 +1,56 @@
 from django.shortcuts import redirect, render
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 # Create your views here.
 
 
 def login(request):
     if request.method == 'POST':
-        print("submitted login")
-        return redirect('login')
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            messages.success(request,"You are now logged in . ")
+            return redirect('dashboard')
+        else:
+            messages.error(request,"Invalid Credentials. ")
+            return redirect('login')
+
     else:
         return render (request,'accounts/login.html')
 
 def register(request):
     if request.method == 'POST':
-        print("submitted register ")
-        messages.error(request,"Testing error message")
-        return redirect('register')
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request,"Username is taken. ")
+                return redirect('register')
+            if User.objects.filter(email=email).exists():
+                messages.error(request,"Email is already registered. ")
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username,first_name=first_name,last_name=last_name,email=email,password=password)
+                # auth.login(request,user)
+                # messages.success(request, "You are now logged in  ")
+                # return redirect('index')
+                user.save()
+                messages.success(request, "You are now Registerd and can log in  ")
+                return redirect('login')
+
+
+        else:
+            messages.error(request,"Passwords Do not match ")
+            return redirect('register')
     else:
         return render (request,'accounts/register.html')
 
